e5debfd274e1e317d9ce537047993e465178a57d
Suhag Al Amin <developersuhag@gmail.com>
2021-12-16 15:02:32 +0600

    initial state

--- src/redux/reducers/bookReducer.js
+++ src/redux/reducers/bookReducer.js
@@ -1,4 +1,12 @@
-function bookReducer(state, action) {
+import books from "../../fakeData/books.json";
+
+const initialState = {
+  discover: books,
+  readingList: [],
+  finishedList: [],
+};
+
+function bookReducer(state = initialState, action) {
   //   if (action.type === "ADD_TO_READING_LIST") {
   //     const newState = [...state, action.payload];
   //     return newState;
@@ -9,11 +17,19 @@ function bookReducer(state, action) {
 
   switch (action.type) {
     case "ADD_TO_READING_LIST": {
-      const newState = [...state, action.payload];
+      const newState = {
+        ...state,
+        readingList: [...state.readingList, action.payload],
+      };
       return newState;
     }
     case "REMOVE_FROM_READING_LIST": {
-      const newState = action.filter((book) => book._id !== action.payload);
+      const newState = {
+        ...state,
+        readingList: state.readingList.filter(
+          (book) => book._id !== action.payload
+        ),
+      };
       return newState;
     }
     default:
