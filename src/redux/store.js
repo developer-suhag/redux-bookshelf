a77fad732ebfe63eae90b487f311e34f2e4f30ff
Suhag Al Amin <developersuhag@gmail.com>
2021-12-16 15:50:24 +0600

    add to reading list

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { combineReducers, createStore } from "redux";
 import bookReducer from "./reducers/bookReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   books: bookReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
